824af1ba7bf2af465be11774dd31f34d72a9a66b
HasibullahAman <hasib.aman7@gmail.com>
2026-01-15 11:48:58 +0430

    done work on products page, done catuch sort and categoris from url and save the changes when we move in app

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -2,6 +2,7 @@ import { ProductsType } from "@/type";
 import { Categories } from "./Categories";
 import ProductCard from "./ProductCard";
 import Link from "next/link";
+import Filter from "./Filter";
 
 
 // Temporary 
@@ -122,6 +123,7 @@ const ProductList = ({ category, params }: { category: string, params: "homepage
     return (
         <div className="w-full">
             <Categories />
+            {params === "products" && <Filter />}
             <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-12">
                 {products.map(product => (
                     <ProductCard key={product.id} product={product} />
