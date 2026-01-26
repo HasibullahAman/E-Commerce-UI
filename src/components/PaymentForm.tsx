d39aa43323c3660cc946c53b6717ada7ad01e4ae
HasibullahAman <hasib.aman7@gmail.com>
2026-01-26 06:10:32 +0430

    done payment form, and install and ready to use zustand for statemanagement

--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -1,84 +1,77 @@
 "use client"
 
-import { shippingFormSchema, ShippingFormInputs } from "@/type"
+import { PaymentFormSchema, PaymentFormInputs } from "@/type"
 import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, ShoppingCart } from "lucide-react";
+import Image from "next/image";
 
-const ShippingForm = () => {
+const PaymentForm = () => {
     const { register, handleSubmit, formState: { errors }, }
-        = useForm<ShippingFormInputs>({ resolver: zodResolver(shippingFormSchema) })
-
+        = useForm<PaymentFormInputs>({ resolver: zodResolver(PaymentFormSchema) })
     const router = useRouter();
-
-    const handlePaymentForm: SubmitHandler<ShippingFormInputs> = (data) => {
+    const handlePaymentForm: SubmitHandler<PaymentFormInputs> = (data) => {
     }
 
     return (
         <form className="flex flex-col gap-4" onSubmit={handleSubmit(handlePaymentForm)}>
             <div className="flex flex-col gap-1">
-                <label htmlFor="name">Name</label>
+                <label htmlFor="cardHolder">Name on Card</label>
                 <input
                     className="border-b border-gray-200 outlin-none text-sm"
                     type="text"
-                    id="name"
+                    id="cardHolder"
                     placeholder="Hasibullah Aman"
-                    {...register("name")}
+                    {...register("cardHolder")}
                 />
-                {errors.name && <p className="text-sm text-red-500">{errors.name.message}</p>}
+                {errors.cardHolder && <p className="text-sm text-red-500">{errors.cardHolder.message}</p>}
             </div>
             <div className="flex flex-col gap-1">
-                <label htmlFor="email">Email</label>
-                <input
-                    className="border-b border-gray-200 outlin-none text-sm"
-                    type="email"
-                    id="email"
-                    placeholder="examle@example.com"
-                    {...register("email")}
-                />
-                {errors.email && <p className="text-sm text-red-500">{errors.email.message}</p>}
-            </div>
-            <div className="flex flex-col gap-1">
-                <label htmlFor="phone">Phone</label>
-                <input
-                    className="border-b border-gray-200 outlin-none text-sm"
-                    type="tel"
-                    id="phone"
-                    placeholder="+93 799 757 131"
-                    {...register("phone")}
-                />
-                {errors.phone && <p className="text-sm text-red-500">{errors.phone.message}</p>}
-            </div>
-            <div className="flex flex-col gap-1">
-                <label htmlFor="address">Address</label>
-                <input
-                    className="border-b border-gray-200 outlin-none text-sm"
-                    type="tel"
-                    id="address"
-                    placeholder="123 Main Street, City, Country"
-                    {...register("address")}
-                />
-                {errors.address && <p className="text-sm text-red-500">{errors.address.message}</p>}
-            </div>
-            <div className="flex flex-col gap-1">
-                <label htmlFor="city">City</label>
+                <label htmlFor="cardNumber">Card Number</label>
                 <input
                     className="border-b border-gray-200 outlin-none text-sm"
                     type="text"
-                    id="city"
-                    placeholder="Kabul"
-                    {...register("city")}
+                    id="cardNumber"
+                    placeholder="1234 5678 9012 3456"
+                    {...register("cardNumber")}
                 />
-                {errors.city && <p className="text-sm text-red-500">{errors.city.message}</p>}
+                {errors.cardNumber && <p className="text-sm text-red-500">{errors.cardNumber.message}</p>}
+            </div>
+            <div className="flex flex-col gap-1">
+                <label htmlFor="expirationDate">Expiration Date</label>
+                <input
+                    className="border-b border-gray-200 outlin-none text-sm"
+                    type="text"
+                    id="expirationDate"
+                    placeholder="MM/YY"
+                    {...register("expirationDate")}
+                />
+                {errors.expirationDate && <p className="text-sm text-red-500">{errors.expirationDate.message}</p>}
+            </div>
+            <div className="flex flex-col gap-1">
+                <label htmlFor="cvv">CVV</label>
+                <input
+                    className="border-b border-gray-200 outlin-none text-sm"
+                    type="text"
+                    id="cvv"
+                    placeholder="123"
+                    {...register("cvv")}
+                />
+                {errors.cvv && <p className="text-sm text-red-500">{errors.cvv.message}</p>}
+            </div>
+            <div className="flex items-center gap-2 mt-4 ">
+                <Image src="/klarna.png" alt="Klarna Logo" width={50} height={25} className="rounded-md cursor-pointer" />
+                <Image src="/cards.png" alt="Klarna Logo" width={50} height={25} className="rounded-md cursor-pointer" />
+                <Image src="/stripe.png" alt="Klarna Logo" width={50} height={25} className="rounded-md cursor-pointer" />
             </div>
             <button
                 className="w-full bg-gray-800 text-white p-2 rounded-lg cursor-pointer
                         flex items-center justify-center gap-2
                         hover:bg-gray-900 transition-all duration-300"
                 type="submit">
-                Continue
-                <ArrowRight className="w-3 h-3" />
+                CheckOut
+                <ShoppingCart className="w-3 h-3" />
             </button>
         </form>
     )
